871a63d5c44c0c9a1da18e6842260edb7f22c00f
BoeNaire <mateimirelionut1@gmail.com>
2024-12-19 13:18:19 +0200

    Update project files: new assets, UI prototype, Objects prototype, DayAndNightCycle

---
+++ scenes/characters/non_playable_character.gd
@@ -0,0 +1,10 @@
+class_name NonPlayableCharacter
+extends CharacterBody2D
+
+@export var min_walk_cycle: int = 2
+@export var max_walk_cycle: int = 6
+
+var walk_cycles: int
+var current_walk_cycle: int
+var idle_animation: String = "idle_front"
+var last_direction: Vector2 = Vector2.ZERO
